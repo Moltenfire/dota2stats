5086ba79632d71317959a8d7578251a26685fc5c
Jonathan Davies <moltenfires@googlemail.com>
2012-08-02 17:10:19 +0100

    add.py adds a user to the group uwcs

--- add.py
+++ add.py
@@ -1,14 +1,21 @@
-import database, sys
+import database, scrapper, sys
 
-insert = "INSERT INTO uwcsplayers(id) VALUES({0}) ON DUPLICATE KEY UPDATE id={0}"
-url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={0}"
+insert = "INSERT INTO uwcsplayers(id, steamid, name) VALUES({0}, {1}, '{2}') ON DUPLICATE KEY UPDATE name='{2}'"
+url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids="
 commands = []
 
 if (len(sys.argv) == 1):
-	print "Please enter one or more uwcs player ids."
+	print "Please enter one or more uwcs 64-bit SteamIDs."
 	sys.exit(1)
 else:
 	for i in sys.argv[1:]:
-		commands.append(insert.format(i))
-		
+		url = ''.join([url, i, ','])
+
+	userdata = scrapper.getDataURL(url)
+	for i in userdata['response']['players']:
+		name = i['personaname']
+		steamid = int(i['steamid'])
+		account = steamid - 0x0110000100000000
+		commands.append(insert.format(account, steamid, name))
+
 database.updateDatabase(commands)
