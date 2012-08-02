4d77ca0b7c26ea3a41cdb61f930ad9de3455cf4d
Jonathan Davies <moltenfires@googlemail.com>
2012-08-02 22:42:19 +0100

    Gets all games of one player

--- parsePlayer.py
+++ parsePlayer.py
@@ -1,23 +1,33 @@
-import scrapper, me
+import database, game, scrapper, sys, time
 
-interval = 86400
-url = ""
+url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=%s&account_id={}&date_min={}&date_max={}"
+sql = "UPDATE uwcsplayers SET lastCheck={} WHERE id={}"
 
 def update(id, lastcheck):
 	currenttime = int(time.time())
-	difference = currenttime - lastcheck
 	
-	i = 0
-	if difference > interval:
-		while lastcheck < currenttime:
-			parseGames(id, lastcheck, lastcheck + interval, currenttime)
-			lastcheck += interval
-			i += 1
-	else:
-		getGames(id, lastcheck, currenttime, currenttime)
+	getGames(id, lastcheck, currenttime)
+	# database.updateDatabase([sql.format(currenttime, id)])
 
-	print i
+
+
 		
-def getGames(id, mintime, maxtime, currenttime, currenttime):
-	pass
+def getGames(id, mintime, maxtime):
+	data = scrapper.getDataURL(url.format(id, mintime, maxtime))
+	if data != None:
+		count = data['result']['total_results']
+
+		if count > 25:
+			for i in data['result']['matches']:
+				game.parseGame(i)
+				newtime = i['start_time']
+				
+			getGames(id, mintime, newtime)
+			
+		else:
+			for i in data['result']['matches']:
+				game.parseGame(i)
 	
+	else:
+		print "No data", data
+		sys.exit(1)
