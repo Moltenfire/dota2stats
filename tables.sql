52090d2eb9e49d52575e87f50880682965c684b7
Jonathan Davies <moltenfires@googlemail.com>
2012-08-02 01:42:39 +0100

    updated tables.sql
    Can run to create whole of dota2 database

--- tables.sql
+++ tables.sql
@@ -1,81 +1,108 @@
-CREATE TABLE `heroes` (
-	`hero_id` SMALLINT(4) UNSIGNED NOT NULL,
-	`hero_name` TINYTEXT NOT NULL,
-	PRIMARY KEY (`hero_id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               5.5.25a - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2012-08-02 01:40:59
+-- --------------------------------------------------------
 
-CREATE TABLE `items` (
-	`item_id` SMALLINT(4) UNSIGNED NOT NULL,
-	`item_name` TINYTEXT NOT NULL,
-	PRIMARY KEY (`item_id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
-CREATE TABLE `match` (
-	`match_id` INT(10) UNSIGNED NOT NULL,
-	`radiant_win` TINYINT(1) UNSIGNED NOT NULL,
-	`duration` INT(6) UNSIGNED NOT NULL,
-	`start_time` INT(11) UNSIGNED NOT NULL,
-	`first_blood` INT(6) UNSIGNED NOT NULL,
-	PRIMARY KEY (`match_id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+-- Dumping database structure for dota2
+CREATE DATABASE IF NOT EXISTS `dota2` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dota2`;
 
-CREATE TABLE `playermatchinfo` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`match_id` INT(10) UNSIGNED NOT NULL,
-	`player_id` INT(10) UNSIGNED NOT NULL,
-	`hero_id` SMALLINT(4) UNSIGNED NOT NULL,
-	`item_0` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`item_1` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`item_2` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`item_3` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`item_4` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`item_5` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`kills` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`deaths` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`assists` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0',
-	`lasthits` MEDIUMINT(5) UNSIGNED NOT NULL DEFAULT '0',
-	`denies` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
-	`level` TINYINT(2) UNSIGNED NOT NULL DEFAULT '0',
-	`gold` MEDIUMINT(7) UNSIGNED NOT NULL DEFAULT '0',
-	`gold_spent` MEDIUMINT(7) UNSIGNED NOT NULL DEFAULT '0',
-	`gpm` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
-	`xpm` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
-	`hero_dmg` MEDIUMINT(6) UNSIGNED NOT NULL DEFAULT '0',
-	`tower_dmg` MEDIUMINT(6) UNSIGNED NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`),
-	INDEX `match` (`match_id`),
-	INDEX `player` (`player_id`),
-	INDEX `hero` (`hero_id`),
-	INDEX `item0` (`item_0`),
-	INDEX `item1` (`item_1`),
-	INDEX `item2` (`item_2`),
-	INDEX `item3` (`item_3`),
-	INDEX `item4` (`item_4`),
-	INDEX `item5` (`item_5`),
-	CONSTRAINT `hero` FOREIGN KEY (`hero_id`) REFERENCES `heroes` (`hero_id`),
-	CONSTRAINT `item0` FOREIGN KEY (`item_0`) REFERENCES `items` (`item_id`),
-	CONSTRAINT `item1` FOREIGN KEY (`item_1`) REFERENCES `items` (`item_id`),
-	CONSTRAINT `item2` FOREIGN KEY (`item_2`) REFERENCES `items` (`item_id`),
-	CONSTRAINT `item3` FOREIGN KEY (`item_3`) REFERENCES `items` (`item_id`),
-	CONSTRAINT `item4` FOREIGN KEY (`item_4`) REFERENCES `items` (`item_id`),
-	CONSTRAINT `item5` FOREIGN KEY (`item_5`) REFERENCES `items` (`item_id`),
-	CONSTRAINT `match` FOREIGN KEY (`match_id`) REFERENCES `match` (`match_id`),
-	CONSTRAINT `player` FOREIGN KEY (`player_id`) REFERENCES `players` (`account_id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
 
-CREATE TABLE `players` (
-	`account_id` INT(10) UNSIGNED NOT NULL,
-	`player_name` TINYTEXT NOT NULL,
-	PRIMARY KEY (`account_id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+-- Dumping structure for table dota2.heroes
+CREATE TABLE IF NOT EXISTS `heroes` (
+  `hero_id` smallint(4) unsigned NOT NULL,
+  `hero_name` tinytext NOT NULL,
+  PRIMARY KEY (`hero_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table dota2.items
+CREATE TABLE IF NOT EXISTS `items` (
+  `item_id` smallint(4) unsigned NOT NULL,
+  `item_name` tinytext NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table dota2.match
+CREATE TABLE IF NOT EXISTS `match` (
+  `match_id` int(10) unsigned NOT NULL,
+  `radiant_win` tinyint(1) unsigned NOT NULL,
+  `duration` int(6) unsigned NOT NULL,
+  `start_time` int(11) unsigned NOT NULL,
+  `first_blood` int(6) unsigned NOT NULL,
+  PRIMARY KEY (`match_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table dota2.playermatchinfo
+CREATE TABLE IF NOT EXISTS `playermatchinfo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `match_id` int(10) unsigned NOT NULL,
+  `player_id` int(10) unsigned NOT NULL,
+  `hero_id` smallint(4) unsigned NOT NULL,
+  `item_0` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item_1` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item_2` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item_3` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item_4` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `item_5` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `kills` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `deaths` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `assists` smallint(4) unsigned NOT NULL DEFAULT '0',
+  `lasthits` mediumint(5) unsigned NOT NULL DEFAULT '0',
+  `denies` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `level` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  `gold` mediumint(7) unsigned NOT NULL DEFAULT '0',
+  `gold_spent` mediumint(7) unsigned NOT NULL DEFAULT '0',
+  `gpm` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `xpm` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `hero_dmg` mediumint(6) unsigned NOT NULL DEFAULT '0',
+  `tower_dmg` mediumint(6) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `match` (`match_id`),
+  KEY `player` (`player_id`),
+  KEY `hero` (`hero_id`),
+  KEY `item0` (`item_0`),
+  KEY `item1` (`item_1`),
+  KEY `item2` (`item_2`),
+  KEY `item3` (`item_3`),
+  KEY `item4` (`item_4`),
+  KEY `item5` (`item_5`),
+  CONSTRAINT `hero` FOREIGN KEY (`hero_id`) REFERENCES `heroes` (`hero_id`),
+  CONSTRAINT `item0` FOREIGN KEY (`item_0`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `item1` FOREIGN KEY (`item_1`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `item2` FOREIGN KEY (`item_2`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `item3` FOREIGN KEY (`item_3`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `item4` FOREIGN KEY (`item_4`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `item5` FOREIGN KEY (`item_5`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `match` FOREIGN KEY (`match_id`) REFERENCES `match` (`match_id`),
+  CONSTRAINT `player` FOREIGN KEY (`player_id`) REFERENCES `players` (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table dota2.players
+CREATE TABLE IF NOT EXISTS `players` (
+  `account_id` int(10) unsigned NOT NULL,
+  `player_name` tinytext NOT NULL,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
