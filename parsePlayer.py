5720612cba067a4ce41f30b73f809bc8a73e2e14
Jonathan Davies <moltenfires@googlemail.com>
2012-08-03 01:27:20 +0100

    Updated for new database update format

--- parsePlayer.py
+++ parsePlayer.py
@@ -1,33 +1,41 @@
 import database, game, scrapper, sys, time
 
 url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=%s&account_id={}&date_min={}&date_max={}"
-sql = "UPDATE uwcsplayers SET lastCheck={} WHERE id={}"
+sql = "UPDATE uwcsplayers SET lastCheck=%s WHERE id=%s"
+games = []
 
 def update(id, lastcheck):
+
+	games = []
 	currenttime = int(time.time())
 	
 	getGames(id, lastcheck, currenttime)
-	# database.updateDatabase([sql.format(currenttime, id)])
+	database.updateDatabase([(sql, (currenttime, id))])
 
 
 
 		
 def getGames(id, mintime, maxtime):
 	data = scrapper.getDataURL(url.format(id, mintime, maxtime))
+	
 	if data != None:
 		count = data['result']['total_results']
 
 		if count > 25:
 			for i in data['result']['matches']:
-				game.parseGame(i)
+				games.append(i['match_id'])
 				newtime = i['start_time']
 				
 			getGames(id, mintime, newtime)
 			
 		else:
 			for i in data['result']['matches']:
+				games.append(i['match_id'])
+
+			
+			for i in games:
 				game.parseGame(i)
-	
+				
 	else:
 		print "No data", data
 		sys.exit(1)
