9dafd02dd38f8bd1806bbab3143766c8d1ff83a3
Jonathan Davies <moltenfires@googlemail.com>
2012-08-06 02:18:20 +0100

    Done more stuff

--- site/dotastats/mainstats/views.py
+++ site/dotastats/mainstats/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from mainstats.models import Uwcsplayers, Mainview, Gamelist
+from math import ceil
 
 
 def index(request):
@@ -14,11 +15,30 @@ def game(request, match_id):
     return HttpResponse("You're at the match %s." % match_id)
     
 def games(request):
-    game_list = Gamelist.objects.all()[:25]
-    return render_to_response('mainstats/games.html', {'game_list': game_list})
+    return games_page(request, 0)
     
 def games_page(request, page):
-    return HttpResponse("You're at the games list page number %s." % page)
+    num_pages = 25
+    try:
+        x = int(page)
+    except:
+        raise Http404
+    game_list = Gamelist.objects.all()
+    total = len(game_list)
+    low = x * num_pages
+    high = low + num_pages
+    if high > total:
+        high = total
+    selected = game_list[low:high] 
+    
+    prev = x - 1 if x > 0 else 0 
+    
+    last = int(ceil((total / num_pages)))
+    
+    next = x + 1 if x < last else last
+    
+    
+    return render_to_response('mainstats/games.html', {'game_list': selected, 'total': total, 'min': low+1, 'max': high, 'prev': prev, 'last': last, 'next': next})
     
 def add(request):
     return HttpResponse("You're at the add player page.")
